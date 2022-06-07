4ee8a5ad633ad03e73d98d9d189b703acf81166d
Taras Garan'ko <garanko.t@gmail.com>
2022-06-07 17:52:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const app = require("./app");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// const { PORT = 3000, DB_HOST } = process.env;
+
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 });
